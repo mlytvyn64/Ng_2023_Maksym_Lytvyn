226f65cf62f3428e8579eddf9044903da31bcde7
Volln34 <litvinmax.06@gmail.com>
2023-10-06 16:31:49 +0300

    Aded first task for first leson

---
+++ leson1/test1.py
@@ -0,0 +1,11 @@
+VozrastDeda=54
+
+
+
+userinput=int(input("Enter your age: "))
+
+print("Yourage: {}, if we willdoubel it: {}".format(userinput, userinput * 2))
+if userinput == VozrastDeda:
+    print("Obideli deda")
+else:
+     print("DKDKDKD")
