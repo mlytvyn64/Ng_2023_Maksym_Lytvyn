5580c5961668f07011f743b23d0703b0c0a7f442
Volln34 <litvinmax.06@gmail.com>
2023-10-14 01:33:40 +0300

    Add tast1 in leson2

---
+++ leson2/Task1/main.py
@@ -0,0 +1,9 @@
+input_str = input("Enter items separated by space: ")
+
+elements = input_str.split()
+
+unique_elements = set(elements)
+
+unique_list = list(unique_elements)
+
+print("", unique_list)
